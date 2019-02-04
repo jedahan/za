03d70be1c347e5d45ecbf9b51dfe8f4beb651115
Jonathan Dahan <hi@jonathan.is>
2019-02-04 11:48:56 -0500

    Get working with single delivery person
    Using sets because they are O(1) insert and size.
    
    Using .stringify because Object equality is based on references
    and we can consider the ordered pair to be a unique address.

--- za.js
+++ za.js
@@ -1,6 +1,18 @@
 const deliver = (directions, deliveryPeople=1) => {
-  const houses = 1
-  return { houses }
+  let pos = [0, 0]
+  const houses = new Set([JSON.stringify(pos)])
+
+  directions.split('').forEach(direction => {
+
+    switch(direction) {
+      case '^': pos[1]++; break;
+      case 'v': pos[1]--; break;
+      case '>': pos[0]++; break;
+      case '<': pos[0]--; break;
+    }
+    houses.add(JSON.stringify(pos))
+  })
+  return { houses: houses.size }
 }
 
 module.exports = { deliver }
