cb901d44937b4d5e70fe7932b92e29fe57efaddf
Jonathan Dahan <hi@jonathan.is>
2019-02-04 12:56:58 -0500

    Fix test input and add some nicer output

--- test.js
+++ test.js
@@ -3,14 +3,20 @@ const { deliver } = require('./za.js')
 
 const tests = {
   1: [['>', 2], ['^>v<', 4], ['^v^v^v^v^v', 2]],
-  2: [['>', 3], ['^>v<', 3], ['^v^v^v^v^v', 11]]
+  2: [['>v', 3], ['^>v<', 3], ['^v^v^v^v^v', 11]]
 }
 
 // TODO: make parallel
 Object.keys(tests).forEach(deliveryPeople => {
   tests[deliveryPeople].forEach(([directions, expectedHouses]) => {
-    const { houses } = deliver(directions)
-    assert.strictEqual(houses, expectedHouses)
+    const { houses, delivered } = deliver(directions, Number(deliveryPeople))
+    try {
+      assert.strictEqual(delivered, expectedHouses)
+      console.log(`✓ ${delivered} houses with ${deliveryPeople} deliveryPeople following '${directions}'`)
+    } catch (e) {
+      console.log(`✘ ${delivered} houses (expected ${expectedHouses}) with ${deliveryPeople} deliveryPeople following '${directions}'`)
+      console.dir({houses})
+    }
   })
 })
 
