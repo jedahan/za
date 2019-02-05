37dbb1efc385531cfec7340b4f17c931b1796a55
Jonathan Dahan <hi@jonathan.is>
2019-02-05 18:18:16 -0500

    Switch to spooning for running tests
    Its relatively minimal, but still supports tap output.

--- tests/delivery.js
+++ tests/delivery.js
@@ -1,23 +1,33 @@
-const assert = require('assert')
 const { deliver } = require('../src/deliver.js')
+const { strictEqual } = require(`assert`)
+const { test, run, exit } = require('spooning');
 
-const tests = {
-  1: [['>', 2], ['^>v<', 4], ['^v^v^v^v^v', 2]],
-  2: [['>v', 3], ['^>v<', 3], ['^v^v^v^v^v', 11]]
-}
+const tests = [
+  {input: { people: 1, directions: '>'}, output: { delivered: 2}},
+  {input: { people: 1, directions: '^>v<'}, output: {delivered: 4}},
+  {input: { people: 1, directions: '^v^v^v^v^v'}, output: {delivered: 2}},
+  {input: { people: 2, directions: '>v'}, output: { delivered: 3}},
+  {input: { people: 2, directions: '^>v<'}, output: {delivered: 3}},
+  {input: { people: 2, directions: '^v^v^v^v^v'}, output: {delivered: 11}},
+  {input: { people: 1, directions: '^v^v^v^v^v'}, output: {delivered: 11}, fail: true},
+]
 
-// TODO: make parallel
-Object.keys(tests).forEach(people => {
-  tests[people].forEach(([directions, expectedHouses]) => {
-    const { houses, delivered } = deliver(directions, Number(people))
-    try {
-      assert.strictEqual(delivered, expectedHouses)
-      console.log(`✓ ${delivered} houses with ${people} pople following '${directions}'`)
-    } catch (e) {
-      console.log(`✘ ${delivered} houses (expected ${expectedHouses}) with ${people} people following '${directions}'`)
-      console.dir({ houses })
+for (let testCase of tests) {
+  const { people, directions } = testCase.input
+  const { delivered: expectedDelivered } = testCase.output
+
+  const description = testCase.fail
+    ? `not ${expectedDelivered} houses with ${people} people following '${directions}'`
+    : `${expectedDelivered} houses with ${people} people following '${directions}'`
+  test(description, cb => {
+    const { delivered } = deliver(directions, people)
+    if (testCase.fail) {
+      try { strictEqual(delivered, expectedDelivered) }
+      catch (e) { cb(null) }
+    } else {
+      cb(strictEqual(delivered, expectedDelivered))
     }
   })
-})
+}
 
-console.log(`tests passed!`)
+run(exit)
